d8487e96c34b07c6e6ffd5933f9b8b58ffc86146
Nick Youngblut <ndy2@cornell.edu>
2016-09-08 13:18:16 -0600

    fixed some tests

--- tests/testthat/test-qPCR.R
+++ tests/testthat/test-qPCR.R
@@ -6,18 +6,39 @@ check_qPCR = function(qPCR, physeq){
                phyloseq::sample_data(physeq) %>% nrow)
 }
 
+make_expr = function(d='norm'){
+  d = tolower(d)
+  if(d=='norm'){
+    control_mean_fun = function(x) dnorm(x, mean=1.70, sd=0.01) * 1e8
+    control_sd_fun = function(x) control_mean_fun(x) / 3
+    treat_mean_fun = function(x) dnorm(x, mean=1.74, sd=0.01) * 1e8
+    treat_sd_fun = function(x) treat_mean_fun(x) / 3
+  } else
+  if(d=='cauchy'){
+    control_mean_fun = function(x) dcauchy(x, location=1.70, scale=0.01) * 1e8
+    control_sd_fun = function(x) control_mean_fun(x) / 3
+    treat_mean_fun = function(x) dcauchy(x, location=1.74, scale=0.01) * 1e8
+    treat_sd_fun = function(x) treat_mean_fun(x) / 3
+  } else {
+    stop('distribution not recognized')
+  }
+
+  L = list(control_mean_fun = control_mean_fun,
+           control_sd_fun = control_sd_fun,
+           treat_mean_fun = treat_mean_fun,
+           treat_sd_fun = treat_sd_fun)
+  return(L)
+}
+
 test_that('qPCR simulation (control/treatment)', {
-  control_mean_fun = function(x) dnorm(x, mean=1.70, sd=0.01) * 1e8
-  control_sd_fun = function(x) control_mean_fun(x) / 3
-  treat_mean_fun = function(x) dnorm(x, mean=1.74, sd=0.01) * 1e8
-  treat_sd_fun = function(x) treat_mean_fun(x) / 3
+  L = make_expr()
 
   qPCR = qPCR_sim(physeq_S2D2,
          control_expr='Substrate=="12C-Con"',
-         control_mean_fun=control_mean_fun,
-         control_sd_fun=control_sd_fun,
-         treat_mean_fun=treat_mean_fun,
-         treat_sd_fun=treat_sd_fun)
+         control_mean_fun=L$control_mean_fun,
+         control_sd_fun=L$control_sd_fun,
+         treat_mean_fun=L$treat_mean_fun,
+         treat_sd_fun=L$treat_sd_fun)
   check_qPCR(qPCR, physeq_S2D2)
 
   p = ggplot(qPCR$summary, aes(Buoyant_density, qPCR_tech_rep_mean,
@@ -30,12 +51,14 @@ test_that('qPCR simulation (control/treatment)', {
 })
 
 test_that('qPCR simulation: replicate gradients', {
+  L = make_expr()
+
   qPCR = qPCR_sim(physeq_rep3,
                   control_expr='Treatment=="12C-Con"',
-                  control_mean_fun=control_mean_fun,
-                  control_sd_fun=control_sd_fun,
-                  treat_mean_fun=treat_mean_fun,
-                  treat_sd_fun=treat_sd_fun)
+                  control_mean_fun=L$control_mean_fun,
+                  control_sd_fun=L$control_sd_fun,
+                  treat_mean_fun=L$treat_mean_fun,
+                  treat_sd_fun=L$treat_sd_fun)
   check_qPCR(qPCR, physeq_rep3)
 
   p = ggplot(qPCR$summary, aes(Buoyant_density, qPCR_tech_rep_mean,
@@ -49,17 +72,14 @@ test_that('qPCR simulation: replicate gradients', {
 
 
 test_that('qPCR simulation (control/treatment); different distributions', {
-  control_mean_fun = function(x) dcauchy(x, location=1.70, scale=0.01) * 1e8
-  control_sd_fun = function(x) control_mean_fun(x) / 3
-  treat_mean_fun = function(x) dcauchy(x, location=1.74, scale=0.01) * 1e8
-  treat_sd_fun = function(x) treat_mean_fun(x) / 3
+  L = make_expr(d='cauchy')
 
   qPCR = qPCR_sim(physeq_S2D2,
                   control_expr='Substrate=="12C-Con"',
-                  control_mean_fun=control_mean_fun,
-                  control_sd_fun=control_sd_fun,
-                  treat_mean_fun=treat_mean_fun,
-                  treat_sd_fun=treat_sd_fun)
+                  control_mean_fun=L$control_mean_fun,
+                  control_sd_fun=L$control_sd_fun,
+                  treat_mean_fun=L$treat_mean_fun,
+                  treat_sd_fun=L$treat_sd_fun)
   check_qPCR(qPCR, physeq_S2D2)
 
   p = ggplot(qPCR$summary, aes(Buoyant_density, qPCR_tech_rep_mean,
@@ -73,6 +93,8 @@ test_that('qPCR simulation (control/treatment); different distributions', {
 
 
 test_that('qPCR simulation warnings', {
+  L = make_expr(d='cauchy')
+
 expect_warning(
   qPCR_sim(physeq_S2D2,
                   control_expr='Substrate=="XXX"',
